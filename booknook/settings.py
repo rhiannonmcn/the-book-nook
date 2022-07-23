8d08bdd6db274e8fae68b4b37d4133f44ebbd812
rhiannonmcn <rhiannonmcn@gmail.com>
2022-07-23 17:39:49 +0100

    Create Book, BookReviews and Category models in models.py in Book App

--- booknook/settings.py
+++ booknook/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rhi-book-nook.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['rhi-book-nook.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'review',
+    'book',
 ]
 
 MIDDLEWARE = [
