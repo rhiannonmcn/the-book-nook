c3bb053486e54305dae2b20d5af6caf5226ee60f
rhiannonmcn <rhiannonmcn@gmail.com>
2022-07-23 22:01:34 +0100

    Create bookList view and bookshelf.html and update url.py

--- book/views.py
+++ book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book
 
-# Create your views here.
+
+
+class BookList(generic.ListView):
+    model = Book
+    queryset = Book.objects.filter(book_approved=True).order_by('-book_created_on')
+    template_name = 'book/bookshelf.html'
+    paginate_by = 18
