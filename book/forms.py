7f206b08ac0087d2cbc948da851ed5094bec894f
rhiannonmcn <rhiannonmcn@gmail.com>
2022-07-30 17:38:27 +0100

    Create context and styles Reviews section

--- book/forms.py
+++ book/forms.py
@@ -6,3 +6,9 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = BookReview
         fields  = ('review_detail',)
+        
+        custom_names = {'email': 'email_address'}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['review_detail'].label = 'Review'
